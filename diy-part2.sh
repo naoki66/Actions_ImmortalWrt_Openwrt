e4223f16c54f886d966a6ebaefad2d5cf4097c73
yuayu <naoki66@163.com>
2023-09-28 21:32:03 +0800

    Update diy-part2.sh
    luci-app-mosdns

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,3 +14,7 @@
 #sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 
 
+# remove v2ray-geodata package from feeds (openwrt-22.03 & master)
+rm -rf feeds/packages/net/v2ray-geodata
+git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
