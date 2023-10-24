d39c1ef4d0ba224b27093cf4a1f1bcfcb7857a9b
yuayu <naoki66@163.com>
2023-10-24 09:44:25 +0800

    Update diy-part2.sh
    clean v2ray-geodata

--- diy-part2.sh
+++ diy-part2.sh
@@ -15,6 +15,8 @@
 
 
 # remove v2ray-geodata package from feeds (openwrt-22.03 & master)
+find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
+find ./ | grep Makefile | grep mosdns | xargs rm -f
 rm -rf feeds/packages/net/v2ray-geodata
 git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
