0ebeb77dc933aa4603a66ec57441465d6a204495
yuayu <naoki66@163.com>
2025-02-23 00:15:22 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,4 +11,4 @@
 #
 
 # 修改默认IP
-#sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.50.1/g' package/base-files/files/bin/config_generate
